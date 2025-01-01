f9a11f8977b4262f46a509b3ab1ebef17e533682
adityaexe <kmradi2002@gmail.com>
2025-01-01 08:24:32 +0530

    add initial components and routing setup for Home, About, Navbar, Hero, and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
